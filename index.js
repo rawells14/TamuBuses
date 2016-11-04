6aac95f97235a3c59671ffc9112f1c34935ed75e
Ryan Wells <rawells14@gmail.com>
2016-11-04 15:31:11 -0500

    More node modules added and basic scraping

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var cheerio = require('cheerio');
+var request = require('request');
 var app = express();
 
 var path = require('path');
@@ -12,21 +14,29 @@ app.set('view engine', 'ejs');
 app.set("port", process.env.PORT || 3000);
 app.engine('html', require('ejs').renderFile);
 
-<!-- Dropdown Trigger -->
- <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
 
- <!-- Dropdown Structure -->
- <ul id='dropdown1' class='dropdown-content'>
-	 <li><a href="#!">one</a></li>
-	 <li><a href="#!">two</a></li>
-	 <li class="divider"></li>
-	 <li><a href="#!">three</a></li>
- </ul>
 app.get("/", function(req, res){
 	res.render("index.html");
 });
 app.get("/buses/3", function(req, res){
-	res.render("bus.html");
+
+	url = 'http://transport.tamu.edu/BusRoutes/Routes.aspx?r=03';
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+						console.log("howdy");
+            var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+
+            var title, release, rating;
+            var json = { title : "", release : "", rating : ""};
+        }
+    })
+		res.send("hello");
 });
 
 io.on('connection', function(socket){
