a20f1542398d93c41847094390361737cb8767e3
Ryan Wells <rawells14@gmail.com>
2016-11-03 22:37:25 -0500

    added index.js and index.html

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var drawnPaths = [];
+
+app.use(express.static('public'));
+app.set("port", process.env.PORT || 3000);
+app.get("/", function(req, res){
+	req.render("index.html");
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
+
+http.listen(app.get("port"), function(){
+	console.log("Listening on port 3000");
+});
