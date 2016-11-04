ed6e4f27d81176482f201380f61098165c4c9ede
Ryan Wells <rawells14@gmail.com>
2016-11-04 14:28:21 -0500

    More work on bus page ui

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ app.set('view engine', 'ejs');
 app.set("port", process.env.PORT || 3000);
 app.engine('html', require('ejs').renderFile);
 
+<!-- Dropdown Trigger -->
+ <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
 
+ <!-- Dropdown Structure -->
+ <ul id='dropdown1' class='dropdown-content'>
+	 <li><a href="#!">one</a></li>
+	 <li><a href="#!">two</a></li>
+	 <li class="divider"></li>
+	 <li><a href="#!">three</a></li>
+ </ul>
 app.get("/", function(req, res){
 	res.render("index.html");
 });
